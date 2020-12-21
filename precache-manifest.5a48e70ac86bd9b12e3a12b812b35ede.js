860150cf0a7dd2ca3c6e10d9fb649c40eaab64af
Mehdi Faraji <mahdifarajideveloper@gmail.com>
2020-12-21 18:43:46 +0330

    added

---
+++ precache-manifest.5a48e70ac86bd9b12e3a12b812b35ede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d73556ee82c2157f4e54366abddd84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ea3048bc07b8c42f7a",
+    "url": "/static/js/2.f40230db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f40230db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1c960c473bec59e113",
+    "url": "/static/js/main.bcd2e84b.chunk.js"
+  },
+  {
+    "revision": "c40b0d5746a5bc2b83a2",
+    "url": "/static/js/runtime-main.e31a4d10.js"
+  }
+]);
